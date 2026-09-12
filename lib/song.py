2e951f1713413ff8cf37576e5d4339f35570237e
IdlePantheon <victormain981@gmail.com>
2026-09-12 23:56:15 +0300

    sound fone

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,46 @@
 class Song:
     pass
+    # Class attributes — shared across ALL instances
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Each of these fires every time a new Song is created
+        self.add_song_to_count()
+        self.add_to_genres()
+        self.add_to_artists()
+        self.add_to_genre_count()
+        self.add_to_artist_count()
+
+    def add_song_to_count(self):
+        Song.count += 1
+
+    def add_to_genres(self):
+        if self.genre not in Song.genres:
+            Song.genres.append(self.genre)
+
+    def add_to_artists(self):
+        if self.artist not in Song.artists:
+            Song.artists.append(self.artist)
+
+    def add_to_genre_count(self):
+        if self.genre in Song.genre_count:
+            Song.genre_count[self.genre] += 1
+        else:
+            Song.genre_count[self.genre] = 1
+
+    def add_to_artist_count(self):
+        if self.artist in Song.artist_count:
+            Song.artist_count[self.artist] += 1
+        else:
+            Song.artist_count[self.artist] = 1
+
+    def __repr__(self):
+        return f"Song('{self.name}', '{self.artist}', '{self.genre}')"
